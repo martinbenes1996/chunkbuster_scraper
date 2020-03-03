0185aa80702a594fa30d22afe99a26e9c4e246c5
Martin Beneš <martinbenes1996@gmail.com>
2020-03-03 22:57:38 +0100

    main

--- src/main.js
+++ src/main.js
@@ -1,3 +1,20 @@
 'use strict'
 
-console.log("Hello, World!")
+
+const axios = require('axios')
+const cheerio = require('cheerio')
+var iconv = require('iconv-lite');
+
+const url = 'https://www.idnes.cz/'
+
+axios(url)
+  .then(response => {
+    const html = response.data;
+    const $ = cheerio.load(html, { decodeEntities: false });
+    let mainHeader = $('.opener').find('h3').html()
+    console.log(mainHeader)
+    let mainHeaderUtf8 = iconv.decode(mainHeader, 'windows-1250')
+    console.log(mainHeaderUtf8)
+    
+  })
+  .catch(console.error);
